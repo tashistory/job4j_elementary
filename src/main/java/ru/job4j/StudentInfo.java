13251c8f1e921127de6738ca761619277b54adfe
semyon <semyon.mihalich@gmail.com>
2023-03-03 16:23:09 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Jebrak Semyon");
+        System.out.println("18.06.1986");
+    }
+}
