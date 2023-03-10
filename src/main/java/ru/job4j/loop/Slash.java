080a87a8bd3f3f3778cc857172812ba533ea1b6a
semyon <semyon.mihalich@gmail.com>
2023-03-10 15:00:49 +0500

    5.9. Крест в псевдографике. [#177182]

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -5,7 +5,7 @@ public class Slash {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
                 boolean left =  row == cell;
-                boolean right = cell == size -1 - row;
+                boolean right = cell == size - 1 - row;
                 if (left) {
                     System.out.print("0");
                 } else if (right) {
