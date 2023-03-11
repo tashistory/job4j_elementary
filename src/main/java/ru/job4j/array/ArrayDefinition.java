14a6a8d3119be0a921e95dd16014f22f6598afcf
semyon <semyon.mihalich@gmail.com>
2023-03-11 10:02:32 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,20 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Сергей Иванов";
+        names[1] = "Алексей Петров";
+        names[2] = "Иван Яковлев";
+        names[3] = "Егений Прокофьев";
 
         System.out.println("Размер массива ages - " + ages.length);
         System.out.println("Размер массива surnames - " + surnames.length);
         System.out.println("Размер массива prices - " + prices.length);
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
