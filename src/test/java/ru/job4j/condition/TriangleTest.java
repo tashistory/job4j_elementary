8550b1bbd302ebeec4c8bfb6e0fcb24fae6dcb6f
semyon <semyon.mihalich@gmail.com>
2023-03-16 14:30:53 +0500

    2.1. Рефакторинг - Площадь треугольника. [#122642 #401338]

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -16,4 +16,14 @@ public class TriangleTest {
         double expected = 8;
         assertThat(rsl).isCloseTo(expected, offset(0.001));
     }
+    @Test
+    public void when00and00and54Thenminus1() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(5, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = -1;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
 }
