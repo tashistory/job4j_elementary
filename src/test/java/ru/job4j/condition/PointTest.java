5e848f8ab5802394353091e727ec37735d50ebea
semyon <semyon.mihalich@gmail.com>
2023-03-16 10:18:09 +0500

    2.1. Рефакторинг - Площадь треугольника. [#122642]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,44 +8,37 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
+
 
     @Test
     public void when01to23then2dot82() {
         double expected = 2.82;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 3);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when46to89then5() {
         double expected = 5;
-        int x1 = 4;
-        int y1 = 6;
-        int x2 = 8;
-        int y2 = 9;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(4, 6);
+        Point b = new Point(8, 9);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when03to57then6dot4() {
         double expected = 6.4;
-        int x1 = 0;
-        int y1 = 3;
-        int x2 = 5;
-        int y2 = 7;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 3);
+        Point b = new Point(5, 7);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
